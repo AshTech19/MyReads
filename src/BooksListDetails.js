0d3bb815dc5b7bc08265e6eb9689d4fa581ad4df
AshTech19 <ayesha19ab@gmail.com>
2021-11-19 20:03:12 -0500

    refactor: Changed into functional component

--- src/BooksListDetails.js
+++ src/BooksListDetails.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+const BooksListDetails = ({book,handleBookShelf})=>{
 
-class BooksListDetail extends Component {
-    render() {
-    const {book} = this.props;
-        return (
+    const bookCover = book.coverLinks ? book.coverLinks.smallThumbnail : null;
+
+    return(
+
+        <ol className="books-grid">
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("book.coverLinks.smallThumbnail")' }}></div>
-                            <div className="book-shelf-changer">
-                                <select>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                            </div>
+                        <div className="book-cover" style={{width:128,height:193,backgroundImage:`url(${bookCover})`}}></div>
+                        <div className="book-shelf-changer">
+                            <select onChange={e=>handleBookShelf(book,e.target.value)} value={book.shelf}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want To Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
                         </div>
+                    </div>
                     <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.author}</div>
+                    <div className="book-authors">{book.authors}</div>
                 </div>
             </li>
-        );
-    }
-}
+        </ol>
+    );
+};
 
 export default BooksListDetail;
